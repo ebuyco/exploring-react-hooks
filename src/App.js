3fb309f8b73567e88d4a7d6cde55fce83322f5ed
ErnieBuyco <ernie.buyco@fmclarity.com>
2018-12-19 22:52:42 +0800

    styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logoedit from './editlogo.png';
+
 import './App.css';
 import Todo from './components/Todo';
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <img style={logo} src={logoedit} alt="logoedit" />
+      
           <Todo />
       </div>
     )
@@ -14,12 +14,6 @@ class App extends Component {
 
 }
 
-const logo = {
-  width: '100%',
-  maxWidth: '20%',
-  height: '100%',
-  margin: '0 auto',
-  
-}
+
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,37 +1,58 @@
 import React, { useState } from 'react';
+import logoedit from '../assets/logoreact-01.svg';
 
 const todo = props => {
        const [todoName, setTodoName] = useState('');
-        
+       const [todoList, setTodoList] = useState([]);
+
        const inputChangeHandler = event => {
             setTodoName(event.target.value)
+       };
+
+       const todoAddHandler = () => {
+                setTodoList(todoList.concat(todoName));
        };
 
     return(
         <React.Fragment>
             <div style={wrapper}>
+            <img style={logo} src={logoedit} alt="logoedit" />
             <input style={text} 
             type="text" 
             placeholders="Todo"
             onChange={inputChangeHandler}
             value={todoName} 
             />
-            <button style={btn} type="button">Add</button>
-            <ul />
+            <button 
+            style={btn}
+             type="button"
+             onClick={todoAddHandler}
+             >Add</button>
+               <ul 
+               style={ulStyle}
+               >
+            {todoList.map(todo => 
+                <li 
+                style={liStyle}
+                key={todo}
+                >{todo}</li>
+              )}
+            </ul>
             </div>
-           
+              
         </React.Fragment>
     );
 };
 
 
 const btn = {
-    backgroundColor: '#2e2e2e', 
+    backgroundColor: '#5362E4', 
     width: '10%',
     color: '#ffffff',
     cursor: 'pointer',
-    padding: '0.8em',
-    fontSize: '1rem'
+    padding: '0.9em',
+    fontSize: '1rem',
+    borderRadius: '0.2rem'
 
 }
 
@@ -40,9 +61,10 @@ const wrapper = {
     textAlign: 'center',
     fontSize: '1.rem',
     marginTop: '2rem',
-    backgroundColor: '#f2f2f2',
+    background: 'linear-gradient(-150deg, #00b89e9c 0%, #0152b5 97%), repeat center',
     width: '70%',
-    padding: '4rem'
+    padding: '4rem',
+    boxShadow: 'rgba(0, 0, 0, 0.15) -2px 1px 20px 0px'
 }
 
 const text = {
@@ -51,5 +73,33 @@ const text = {
     width: '60%',
     fontSize: '1rem'
 }
+
+const logo = {
+    width: '100%',
+    maxWidth: '25%',
+    height: '100%',
+    textAlign: 'center',
+    paddingBottom: '2rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0 auto'
+    
+  }
+
+  const ulStyle = {
+      color: '#ffffff',
+      listStyle: 'none'
+
+  }
+
+  const liStyle = {
+        color: '#ffffff',
+        listStyle: 'none',
+        fontWeight: '400',
+        fontSize: '1rem'
+
+
+  }
 export default todo;
 
